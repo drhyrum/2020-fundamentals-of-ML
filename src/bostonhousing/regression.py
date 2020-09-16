ef315683c5e03b0333303a10a8e85119c446778f
Hyrum Anderson <hyruma@microsoft.com>
2020-09-16 12:45:24 -0600

    prep for another lesson

---
+++ src/bostonhousing/regression.py
@@ -0,0 +1,4 @@
+from sklearn.datasets import load_boston
+
+data = load_boston()  # see: https://scikit-learn.org/stable/modules/generated/sklearn.datasets.load_boston.html#sklearn.datasets.load_boston
+X
